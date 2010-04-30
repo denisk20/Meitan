3be4fe50adf21ab9a61f063cd1815c9ed6dfca1f
Denis Knjazhev <denis.k1985@gmail.com>
2010-04-30 18:11:28 +0300

    in the middle of categories...

--- Web/src/main/java/com/meitan/lubov/services/util/FileUploadHandler.java
+++ Web/src/main/java/com/meitan/lubov/services/util/FileUploadHandler.java
@@ -1,13 +1,17 @@
 package com.meitan.lubov.services.util;
 
 import org.springframework.binding.message.MessageContext;
+import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
+import javax.servlet.ServletContext;
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Date: Apr 19, 2010
@@ -15,10 +19,20 @@ import java.io.Serializable;
  *
  * @author denisk
  */
-public class FileUploadHandler implements Serializable {
+public class FileUploadHandler implements Serializable, ServletContextAware {
 	private final static String FILE_PARAM_NAME = "file";
+	private static final String UPLOAD_DIR_NAME = "uploaded";
+	private static final String IMAGE_PREFIX = "img";
+	private static final String DELIM = "_";
 
-	public void processFile(MessageContext messageContext, RequestContext requestContext) throws IOException {
+	private ServletContext servletContext;
+
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+	public void processFile(String entityName, String id, RequestContext requestContext) throws IOException {
 		MultipartFile file = requestContext.getRequestParameters().getMultipartFile(FILE_PARAM_NAME);
     	file.getBytes();
 
@@ -26,8 +40,19 @@ public class FileUploadHandler implements Serializable {
 			throw new IllegalArgumentException("File was null");
 		}
 		if (file.getSize() > 0) {
-			// data was uploaded
-			requestContext.getFlashScope().put("file", new String(file.getBytes()));
-		} 
+			String uploadedFolderPath = servletContext.getRealPath(UPLOAD_DIR_NAME);
+			File uploadDir = new File(uploadedFolderPath);
+			if (!uploadDir.exists()) {
+				throw new IllegalStateException("Upload directory doesn't exist: " + UPLOAD_DIR_NAME);
+			}
+			if (!uploadDir.isDirectory()) {
+				throw new IllegalStateException("Upload directory is not a directory: " + UPLOAD_DIR_NAME);
+
+			}
+			//rename it
+			String newName = IMAGE_PREFIX + DELIM + entityName + DELIM + id;
+			file.transferTo(new File(uploadDir, newName));
+		}
 	}
+
 }
