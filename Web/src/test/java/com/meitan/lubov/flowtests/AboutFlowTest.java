8c4055f1ac7f5e6805f422e65cba8cfc9bd7ca34
Denis Knjazhev <denis.k1985@gmail.com>
2010-06-17 14:13:24 +0300

    Added test for 'about' flow. using new version of facelets\spring security taglib.

---
+++ Web/src/test/java/com/meitan/lubov/flowtests/AboutFlowTest.java
@@ -0,0 +1,78 @@
+package com.meitan.lubov.flowtests;
+
+import com.meitan.lubov.model.persistent.Product;
+import com.meitan.lubov.services.dao.ProductDao;
+import com.meitan.lubov.services.dao.jpa.impl.JpaProductDao;
+import com.meitan.lubov.services.util.DenisConversionService;
+import com.meitan.lubov.services.util.FileUploadHandler;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.webflow.config.FlowDefinitionResource;
+import org.springframework.webflow.config.FlowDefinitionResourceFactory;
+import org.springframework.webflow.test.MockExternalContext;
+import org.springframework.webflow.test.MockFlowBuilderContext;
+import org.springframework.webflow.test.execution.AbstractXmlFlowExecutionTests;
+
+import javax.faces.model.DataModel;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * @author denis_k
+ *         Date: 16.06.2010
+ *         Time: 17:59:34
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:testsSetup.xml"})
+@TestExecutionListeners({TransactionalTestExecutionListener.class, DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class})
+@Transactional
+public class AboutFlowTest extends AbstractXmlFlowExecutionTests {
+	@Autowired
+	private ProductDao testProductDao;
+
+	private FileUploadHandler fileUploadHandler = new FileUploadHandler();
+
+	@Override
+	protected FlowDefinitionResource getResource(FlowDefinitionResourceFactory resourceFactory) {
+		return resourceFactory.createFileResource("Web/src/main/webapp/WEB-INF/flows/about/about-flow.xml");
+	}
+
+	@Override
+	protected void configureFlowBuilderContext(MockFlowBuilderContext builderContext) {
+		builderContext.registerBean("productDao", testProductDao);
+		builderContext.registerBean("fileUploadHandler", fileUploadHandler);
+		builderContext.getFlowBuilderServices().setConversionService(new DenisConversionService());
+	}
+
+	@Override
+	protected FlowDefinitionResource[] getModelResources(FlowDefinitionResourceFactory resourceFactory) {
+		Resource globalFlowResource = new FileSystemResource(new File("Web/src/main/webapp/WEB-INF/flows/global/global-flow.xml"));
+		Resource goodFlowResource = new FileSystemResource(new File("Web/src/main/webapp/WEB-INF/flows/good/good-flow.xml"));
+		return new FlowDefinitionResource[] {
+				new FlowDefinitionResource("global", globalFlowResource, null),
+				new FlowDefinitionResource("good", goodFlowResource, null),
+		};
+	}
+
+	@Test
+	public void testStartAboutFlow() {
+		MockExternalContext context = new MockExternalContext();
+
+		startFlow(context);
+
+		assertCurrentStateEquals("aboutUs");
+		DataModel newProductsDataModel = (DataModel) getRequiredFlowAttribute("newProducts");
+		ArrayList<Product> newProducts = (ArrayList<Product>) newProductsDataModel.getWrappedData();
+		assertEquals("Wrong number of products", 1, newProducts.size());
+	}
+}
