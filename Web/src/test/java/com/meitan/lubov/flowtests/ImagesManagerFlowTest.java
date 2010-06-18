e4732b5be6cd7c580cc5b2168b903d424fdf755b
Denis Knjazhev <denis.k1985@gmail.com>
2010-06-18 18:30:17 +0300

    started work on ImageManager flow test

---
+++ Web/src/test/java/com/meitan/lubov/flowtests/ImagesManagerFlowTest.java
@@ -0,0 +1,10 @@
+package com.meitan.lubov.flowtests;
+
+/**
+ * @author denis_k
+ *         Date: 18.06.2010
+ *         Time: 18:24:46
+ */
+public class ImagesManagerFlowTest extends AbstractFlowIntegrationTest{
+	
+}
