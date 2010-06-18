5ac81bdc4911c438a357cffa3d206b595337ddce
Denis Knjazhev <denis.k1985@gmail.com>
2010-06-18 18:22:11 +0300

    addet test for editGood flow

---
+++ Web/src/test/java/com/meitan/lubov/flowtests/EditGoodFlowTest.java
@@ -0,0 +1,114 @@
+package com.meitan.lubov.flowtests;
+
+import com.meitan.lubov.model.persistent.Category;
+import com.meitan.lubov.model.persistent.Product;
+import com.meitan.lubov.services.dao.CategoryDao;
+import com.meitan.lubov.services.dao.ProductDao;
+import com.meitan.lubov.services.util.DenisConversionService;
+import com.meitan.lubov.services.util.Selectable;
+import com.meitan.lubov.services.util.SelectableImpl;
+import com.meitan.lubov.services.util.selectors.CategoriesSelector;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.webflow.config.FlowDefinitionResource;
+import org.springframework.webflow.config.FlowDefinitionResourceFactory;
+import org.springframework.webflow.core.collection.LocalAttributeMap;
+import org.springframework.webflow.core.collection.MutableAttributeMap;
+import org.springframework.webflow.test.MockExternalContext;
+import org.springframework.webflow.test.MockFlowBuilderContext;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author denis_k
+ *         Date: 18.06.2010
+ *         Time: 15:56:23
+ */
+public class EditGoodFlowTest extends AbstractFlowIntegrationTest{
+	@Autowired
+	private CategoryDao testCategoryDao;
+
+	@Autowired
+	private ProductDao testProductDao;
+
+	@Override
+	protected FlowDefinitionResource getResource(FlowDefinitionResourceFactory resourceFactory) {
+		return resourceFactory.createFileResource(rootPath + "/Web/src/main/webapp/WEB-INF/flows/editGood/editGood-flow.xml");
+	}
+
+	@Override
+	protected void configureFlowBuilderContext(MockFlowBuilderContext builderContext) {
+		builderContext.registerBean("productDao", testProductDao);
+		builderContext.registerBean("categoryDao", testCategoryDao);
+//		todo
+//		builderContext.getFlowBuilderServices().setConversionService(new DenisConversionService());
+	}
+
+	@Test
+	public void testEditGood() {
+		Category newCategory = new Category("myCategory");
+		testCategoryDao.makePersistent(newCategory);
+
+		Product p = testProductDao.findAll().get(0);
+
+		boolean initialNew = p.isNew();
+
+		MockExternalContext context = new MockExternalContext();
+		MutableAttributeMap input = new LocalAttributeMap();
+		input.put("productId", p.getId());
+
+		startFlow(input, context);
+
+		assertCurrentStateEquals("editGood");
+
+		CategoriesSelector categoriesSelector = (CategoriesSelector) getFlowScope().getRequired("categoriesSelector");
+		HashSet<Category> selectedCategories = new HashSet<Category>();
+
+		for (Selectable<Category> s : categoriesSelector.getItems()) {
+			if (s.isSelected()) {
+				selectedCategories.add(s.getItem());
+			}
+		}
+
+		int selectedItemsCount = selectedCategories.size();
+		assertTrue(selectedItemsCount > 0);
+		
+		assertEquals("Wrong items selected", p.getCategories(), selectedCategories);
+
+		Product productFromFlowScope = (Product) getFlowScope().getRequired("product", Product.class);
+
+		assertEquals("Products don't match", p, productFromFlowScope);
+
+		categoriesSelector.addItem(newCategory, true);
+
+		String newProductName = "newProductName";
+		p.setName(newProductName);
+		p.setNew(! initialNew);
+
+		context.setEventId("save");
+
+		resumeFlow(context);
+
+		Product reloadedProduct = testProductDao.findById(p.getId());
+		assertEquals("Wrong product name", newProductName, reloadedProduct.getName());
+		assertEquals("Wrong NEW status of a product", ! initialNew, reloadedProduct.isNew());
+		assertTrue("Category wasn't attached to a product", reloadedProduct.getCategories().contains(newCategory));
+		
+		assertCurrentStateEquals("save");
+	}
+
+	@Test
+	public void testBack() {
+		setCurrentState("editGood");
+
+		MockExternalContext context = new MockExternalContext();
+		context.setEventId("back");
+
+		resumeFlow(context);
+
+		assertFlowExecutionEnded();
+		assertFlowExecutionOutcomeEquals("cancel");
+	}
+}
