a0da523979a98c673f28bd3728a1d67ae4afd2f2
Denis Knjazhev <denis.k1985@gmail.com>
2010-07-11 12:27:48 +0300

    finished creation of user's registry service

--- Web/src/main/java/com/meitan/lubov/services/util/SecurityService.java
+++ Web/src/main/java/com/meitan/lubov/services/util/SecurityService.java
@@ -1,7 +1,13 @@
 package com.meitan.lubov.services.util;
 
 import com.meitan.lubov.model.persistent.Client;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.GrantedAuthorityImpl;
 import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.event.authentication.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.providers.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.webflow.context.ExternalContext;
@@ -15,18 +21,24 @@ import java.io.IOException;
  *         Time: 15:37:25
  */
 @Service("securityService")
-public class SecurityService {
+public class SecurityService implements ApplicationContextAware{
+	private ApplicationContext applicationContext;
 	public void authenticateUser(Client user) {
+		GrantedAuthority[] authorities = 
+				new GrantedAuthority[] {new GrantedAuthorityImpl("ROLE_CLIENT")};
+
 		UsernamePasswordAuthenticationToken token =
-				new UsernamePasswordAuthenticationToken(user.getLogin(), user.getPassword());
+				new UsernamePasswordAuthenticationToken(
+						user.getLogin(), user.getPassword(), authorities);
 		SecurityContextHolder.getContext().setAuthentication(token);
+
+		if (this.applicationContext != null) {
+            applicationContext.publishEvent(new InteractiveAuthenticationSuccessEvent(token, this.getClass()));
+        }
 	}
 
-	public void doLogin(RequestContext requestContext) {
-		ExternalContext context = requestContext.getExternalContext();
-
-		
-		return ;
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
 	}
-
 }
