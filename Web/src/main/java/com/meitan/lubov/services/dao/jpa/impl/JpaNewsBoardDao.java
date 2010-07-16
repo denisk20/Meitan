ea9158299efc3a43f3ead62aba62bccdaa4f01bc
Denis Knjazhev <denis.k1985@gmail.com>
2010-07-17 00:04:07 +0300

    added rich editor

--- Web/src/main/java/com/meitan/lubov/services/dao/jpa/impl/JpaNewsBoardDao.java
+++ Web/src/main/java/com/meitan/lubov/services/dao/jpa/impl/JpaNewsBoardDao.java
@@ -1,10 +1,14 @@
 package com.meitan.lubov.services.dao.jpa.impl;
 
+import com.meitan.lubov.model.persistent.BoardItem;
 import com.meitan.lubov.model.persistent.NewsBoard;
 import com.meitan.lubov.services.dao.NewsBoardDao;
 import com.meitan.lubov.services.dao.jpa.JpaDao;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author denis_k
@@ -14,4 +18,11 @@ import org.springframework.stereotype.Service;
 @Repository
 @Service("newsBoardDao")
 public class JpaNewsBoardDao extends JpaDao<NewsBoard, Long> implements NewsBoardDao{
+	@Override
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	public List<BoardItem> getAll() {
+		List<BoardItem> result = em.createNamedQuery("getAll").getResultList();
+		return result;
+	}
 }
