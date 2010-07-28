f7e6c9048cdb9b8a540c22f026d832992c16dc5d
denis_k <denis.k1985@gmail.com>
2010-07-28 12:44:57 +0300

    made Image tag work. Made NewsBoard to be enum-aware. Created unit test for NewsBoard

---
+++ Model/src/main/java/com/meitan/lubov/model/BoardType.java
@@ -0,0 +1,12 @@
+package com.meitan.lubov.model;
+
+/**
+ * Date: Jul 28, 2010
+ * Time: 10:12:04 AM
+ *
+ * @author denisk
+ */
+public enum BoardType {
+	ABOUT,
+	FOR_CONSULTANTS
+}
--- Web/src/test/java/com/meitan/lubov/NewsBoardIntegrationTest.java
+++ Web/src/test/java/com/meitan/lubov/NewsBoardIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.meitan.lubov;
 
+import com.meitan.lubov.model.BoardType;
 import com.meitan.lubov.model.persistent.Authority;
+import com.meitan.lubov.model.persistent.BoardItem;
 import com.meitan.lubov.model.persistent.Client;
 import com.meitan.lubov.model.persistent.NewsBoard;
 import com.meitan.lubov.services.dao.AuthorityDao;
@@ -27,13 +29,34 @@ public class NewsBoardIntegrationTest extends GenericIntegrationTest <NewsBoard>
 	@Autowired
 	private ClientDao testClientDao;
 
+	private static final int EXPECTED_ABOUT_ITEMS_COUNT = 2;
+	private static final int EXPECTED_CONSULTANT_ITEMS_COUNT = 3;
+
 	@Override
 		protected void setUpBeanNames() {
-		beanNames.add("ent_board");
+		beanNames.add("ent_AboutBoard");
+		beanNames.add("ent_ConsultantsBoard");
 	}
 
 	@Override
 	protected Dao<NewsBoard, Long> getDAO() {
 		return testNewsBoardDao;
+	}
+
+	@Override
+	protected void compareAdditionalProperties(NewsBoard beanFromSpring, NewsBoard beanFromDB) {
+		assertEquals(beanFromSpring.getBoardType(), beanFromDB.getBoardType());
+	}
+
+	@Test
+	public void testGetForBoard() {
+		BoardType aboutType = BoardType.ABOUT;
+		BoardType consultantsType = BoardType.FOR_CONSULTANTS;
+
+		List<BoardItem> aboutItems = testNewsBoardDao.getForBoard(aboutType);
+		assertEquals(EXPECTED_ABOUT_ITEMS_COUNT, aboutItems.size());
+
+		List<BoardItem> consultantItems = testNewsBoardDao.getForBoard(consultantsType);
+		assertEquals(EXPECTED_CONSULTANT_ITEMS_COUNT, consultantItems.size());
 	}
 }
