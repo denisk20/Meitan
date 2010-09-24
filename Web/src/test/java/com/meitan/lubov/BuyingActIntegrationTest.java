43fe7120ebaba01250b217700963a7c2e70403c5
Denis Knjazhev <denis.k1985@gmail.com>
2010-09-24 12:38:45 +0300

    finished unit test for checkout

--- Web/src/test/java/com/meitan/lubov/BuyingActIntegrationTest.java
+++ Web/src/test/java/com/meitan/lubov/BuyingActIntegrationTest.java
@@ -1,19 +1,23 @@
 package com.meitan.lubov;
 
 import com.meitan.lubov.model.persistent.BuyingAct;
+import com.meitan.lubov.model.persistent.Client;
 import com.meitan.lubov.model.persistent.ShoppingCartItem;
 import com.meitan.lubov.services.dao.BuyingActDao;
+import com.meitan.lubov.services.dao.ClientDao;
 import com.meitan.lubov.services.dao.Dao;
 import com.meitan.lubov.services.dao.ShoppingCartItemDao;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.PersistenceException;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author denis_k
@@ -26,6 +30,9 @@ public class BuyingActIntegrationTest extends GenericIntegrationTest<BuyingAct> 
 	
 	@Autowired
 	private ShoppingCartItemDao testShoppingCartItemDao;
+
+	@Autowired
+	private ClientDao testClientDao;
 
     @Override
     protected void setUpBeanNames() {
@@ -67,5 +74,16 @@ public class BuyingActIntegrationTest extends GenericIntegrationTest<BuyingAct> 
 		List<BuyingAct> acts = testBuyingActDao.findForCartItem(item.getId());
 
 		assertEquals(1, acts.size());
+	}
+
+	@Test
+	public void testFindForLogin() {
+		Client c = testClientDao.findAll().get(0);
+		String login = c.getLogin();
+		//assume the client has some boughts
+		final Set<BuyingAct> initialActs = c.getPurchases();
+		final List<BuyingAct> foundActs = testBuyingActDao.findForLogin(login);
+
+		assertEquals(new ArrayList(initialActs), foundActs);
 	}
 }
