6ebe9f7865296d54c20eb151715196c6a30b3219
denis_k <denis.k1985@gmail.com>
2010-11-16 01:04:52 +0200

    good flow enhansments

--- Web/src/test/java/com/meitan/lubov/flowtests/GoodFlowTest.java
+++ Web/src/test/java/com/meitan/lubov/flowtests/GoodFlowTest.java
@@ -6,12 +6,18 @@ import com.meitan.lubov.services.util.DenisConversionService;
 import com.meitan.lubov.services.util.Utils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.webflow.config.FlowDefinitionResource;
 import org.springframework.webflow.config.FlowDefinitionResourceFactory;
 import org.springframework.webflow.core.collection.LocalAttributeMap;
 import org.springframework.webflow.core.collection.MutableAttributeMap;
 import org.springframework.webflow.test.MockExternalContext;
 import org.springframework.webflow.test.MockFlowBuilderContext;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author denis_k
@@ -34,6 +40,23 @@ public class GoodFlowTest extends AbstractFlowIntegrationTest{
 		builderContext.registerBean("productDao", testProductDao);
 		builderContext.registerBean("utils", new Utils());
 		builderContext.getFlowBuilderServices().setConversionService(new DenisConversionService());
+	}
+	@Override
+	protected FlowDefinitionResource[] getModelResources(FlowDefinitionResourceFactory resourceFactory) {
+		FlowDefinitionResource[] superResources = super.getModelResources(resourceFactory);
+
+		ArrayList<FlowDefinitionResource> result = new ArrayList<FlowDefinitionResource>();
+
+		result.addAll(Arrays.asList(superResources));
+
+//		Resource localFlowResource1 = new FileSystemResource(new File(rootPath + "/Web/src/main/webapp/WEB-INF/flows/viewGoods/viewGoods-flow.xml"));
+		Resource localFlowResource2 = new FileSystemResource(new File(rootPath + "/Web/src/main/webapp/WEB-INF/flows/baseGood/baseGood-flow.xml"));
+		Resource localFlowResource3 = new FileSystemResource(new File(rootPath + "/Web/src/main/webapp/WEB-INF/flows/abstractEditable/abstractEditable-flow.xml"));
+		result.add(new FlowDefinitionResource("baseGood", localFlowResource2, null));
+//		result.add(new FlowDefinitionResource("viewGoods", localFlowResource1, null));
+		result.add(new FlowDefinitionResource("abstractEditable", localFlowResource3, null));
+
+		return result.toArray(new FlowDefinitionResource[0]);
 	}
 
 	@Test
