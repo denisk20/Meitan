454c2ed4dc09d629832b6b48014712cf384bc426
Denis Knjazhev <denis.k1985@gmail.com>
2010-12-17 19:14:46 +0200

    tweaking UI javascript tests

--- Web/src/main/webapp/js/newItems.js
+++ Web/src/main/webapp/js/newItems.js
@@ -1,14 +1,22 @@
 		var items;
-		var itemsToDisplayCount = 3;
+		var itemsToDisplayCount = 2;
 		var lastUsedItems;
 
+        Array.prototype.shuffle = function() {
+            var s = [];
+            while (this.length) s.push(this.splice(Math.random() * this.length, 1));
+            while (s.length) this.push(s.pop());
+            return this;
+        };
+
 		function main() {
+            dojo.require("dojo.fx");
 			items = dojo.query(".imageItem");
 			lastUsedItems = items;
 
 			shrinkDivs();
 			prepareNewGoods();
-			setInterval('prepareNewGoods()', 3000);
+			setInterval('prepareNewGoods()', 1500);
 		}
 
 		function shrinkDivs() {
@@ -24,51 +32,36 @@
 			}
 		}
 
-		function getUniqueIndex(usedIndexes, upperBound) {
-			var index = Math.floor(Math.random() * (upperBound + 1));
-			for (var i = 0; i < usedIndexes.length; i++) {
-				if (usedIndexes[i] == index) {
-					return getUniqueIndex(usedIndexes, upperBound);
-				}
-			}
-			return index;
-		}
-
-		function prepareNewGoods() {
+        function prepareNewGoods() {
 			var itemsToDisplay = [];
 			if (items.length <= itemsToDisplayCount) {
 				itemsToDisplay = items;
 			} else {
-				var usedIndexes = [];
-				for (var i = 0; i < itemsToDisplayCount; i++) {
-					var index = getUniqueIndex(usedIndexes, items.length);
-					usedIndexes[i] = index;
-				}
+                var indexesToUse = [];
+                for (var i = 0; i < items.length; i++) {
+                    indexesToUse[i] = i;
+                }
+                indexesToUse.shuffle();
 
+                var splicedIndexesToUse = indexesToUse.slice(0, itemsToDisplayCount);
 				for (var i = 0; i < itemsToDisplayCount; i++) {
-					itemsToDisplay[i] = items[usedIndexes[i]];
+					itemsToDisplay[i] = items[splicedIndexesToUse[i]];
 				}
 			}
 
 			for(var i=0; i<lastUsedItems.length; i++) {
 				var item = lastUsedItems[i];
-//				var animFadeOut = dojo.fadeOut({node: item ,duration: 1000});
-				var animFadeOut = dojo.fadeOut({node: item ,duration: 1000, onEnd: function() {
-					item.style.display = 'none'
-				}});
-
+				var animFadeOut = dojo.fadeOut({node: item ,duration: 500});
 				animFadeOut.play();
-			}
+                dojo.style(item, 'display', 'none');
+            }
 
 			for (var i = 0; i < itemsToDisplay.length; i++) {
 				var item = itemsToDisplay[i];
-//				var animFadeIn = dojo.fadeIn({node: item ,duration: 1000});
-				var animFadeIn = dojo.fadeIn({node: item ,duration: 1000, beforeBegin: function() {
-					item.style.display = 'block'
-				}});
-
+				var animFadeIn = dojo.fadeIn({node: item ,duration: 500});
 				animFadeIn.play();
-			}
+                var animNoDisplay = dojo.style(item, 'display', 'block');
+            }
 
 			lastUsedItems = itemsToDisplay;
 		}
