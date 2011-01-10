a83d9139a41a682cf0b27873fb198c41c4c9a330
Denis Knjazhev <denis.k1985@gmail.com>
2011-01-10 15:15:54 +0200

    finished unit test for syncronizer

--- Web/src/test/java/com/meitan/lubov/services/MeitanSyncronizerImplTest.java
+++ Web/src/test/java/com/meitan/lubov/services/MeitanSyncronizerImplTest.java
@@ -1,5 +1,8 @@
 package com.meitan.lubov.services;
 
+import com.meitan.lubov.model.persistent.Category;
+import com.meitan.lubov.model.persistent.Image;
+import com.meitan.lubov.model.persistent.Product;
 import com.meitan.lubov.services.dao.CategoryDao;
 import com.meitan.lubov.services.dao.ProductDao;
 import com.meitan.lubov.services.util.Utils;
@@ -13,9 +16,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.List;
 import javax.xml.xpath.XPathExpressionException;
 import static org.junit.Assert.*;
 
@@ -39,18 +45,58 @@ public class MeitanSyncronizerImplTest extends AbstractTransactionalJUnit4Spring
 
 	@Test
 	public void getCategories() throws IOException, XPathExpressionException {
-		int initialCategoriesSize = testCategoryDao.findAll().size();
-		int initialProductsSize = testProductDao.findAll().size();
-		MeitanSyncronizer syncronizer = (MeitanSyncronizer) applicationContext.getBean("testMeitanSyncronizerImpl");
-		final String testsPath = "file://" + utils.getHomePath() + TESTS_ROOT;
-		syncronizer.setUrl(testsPath + "meitan.ru.catalog.html");
-		syncronizer.setPrefixUrl("");
-		syncronizer.setProductsUrl("");
-		syncronizer.setFilePrefix(testsPath);
-		syncronizer.sync();
+		HashSet<File> filesToDelete = new HashSet<File>();
+		try {
+			final List<Category> initialCategories = testCategoryDao.findAll();
+			int initialCategoriesSize = initialCategories.size();
+			final List<Product> initialProducts = testProductDao.findAll();
+			int initialProductsSize = initialProducts.size();
+			MeitanSyncronizer syncronizer = (MeitanSyncronizer) applicationContext.getBean("testMeitanSyncronizerImpl");
+			final String testsPath = "file://" + utils.getHomePath() + TESTS_ROOT;
+			syncronizer.setUrl(testsPath + "meitan.ru.catalog.html");
+			syncronizer.setPrefixUrl("");
+			syncronizer.setProductsUrl("");
+			syncronizer.setFilePrefix(testsPath);
+			syncronizer.sync();
 
-		assertTrue(testCategoryDao.findAll().size() == initialCategoriesSize + 1);
-		assertTrue(testProductDao.findAll().size() == initialProductsSize + 2);
+			final List<Category> finalCategories = testCategoryDao.findAll();
+			final List<Product> finalProducts = testProductDao.findAll();
+			assertTrue(finalCategories.size() == initialCategoriesSize + 1);
+			assertTrue(finalProducts.size() == initialProductsSize + 2);
+
+			finalCategories.removeAll(initialCategories);
+			finalProducts.removeAll(initialProducts);
+
+			Category newCategory = finalCategories.get(0);
+			assertNotNull(newCategory.getName());
+			assertNotNull(newCategory.getDescription());
+			assertFalse(newCategory.getName().isEmpty());
+			assertFalse(newCategory.getDescription().isEmpty());
+
+			for (Product p : finalProducts) {
+				assertTrue(newCategory.getProducts().contains(p));
+				for (Image i : p.getImages()) {
+					final File productImageFile = new File(utils.getImageUploadDirectoryPath() + i.getUrl());
+					assertTrue(productImageFile.exists());
+					filesToDelete.add(productImageFile);
+				}
+				assertNotNull(p.getName());
+				assertNotNull(p.getDescription());
+				assertFalse(p.getName().isEmpty());
+				assertFalse(p.getDescription().isEmpty());
+				assertNotNull(p.getAvatar());
+			}
+
+			final Image categoryImage = newCategory.getImage();
+			assertNotNull(categoryImage);
+			final File categoryImageFile = new File(utils.getImageUploadDirectoryPath() + categoryImage.getUrl());
+			filesToDelete.add(categoryImageFile);
+			assertTrue(categoryImageFile.exists());
+		} finally {
+			for (File f : filesToDelete) {
+				assertTrue(f.delete());
+			}
+		}
 	}
 
 }
